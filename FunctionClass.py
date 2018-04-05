c06d7fae0b65ab8fc614cdaf2ee9e72947eb2fe3
Anis Mohammed Vengasseri <anis.mhd@gmail.com>
2018-04-05 13:05:24 +0530

    New type of class fuction for Polynomial Function added

--- FunctionClass.py
+++ FunctionClass.py
@@ -1,8 +1,9 @@
 '''
 AAA
 '''
-from numpy import linspace,zeros,log
+from numpy import linspace,zeros,log, array
 import matplotlib.pyplot as plt
+
 class ContinuousPolynomialFunction():
 	"""Continuous Polynomial Function of
 			1 - x, PGAref
@@ -55,58 +56,49 @@ class ContinuousPolynomialFunction():
 			if i <= self.N[5]-1:
 				A = A + self.EL[i]*(log(z+self.EL[0]))**i
 		return A
-	def generate_figure(self,**para):
-		'''
-			x_fix : fixed value for 'x'
-			x_low : lower bound for 'x'
-			x_up  : upper bound for 'x'
-			y_fix : fixed value for 'y'
-			y_low : lower bound for 'y'
-			y_up  : upper bound for 'y'
-			z_fix : fixed value for 'z'
-			z_low : lower bound for 'z'
-			z_up  : upper bound for 'z'
-		'''
-		x_fix = para.get('x_fix')
-		x_low = para.get('x_low')
-		x_up = para.get('x_up')
-		y_fix = para.get('y_fix')
-		y_low = para.get('y_low')
-		y_up = para.get('y_up')
-		z_fix = para.get('z_fix')
-		z_low = para.get('z_low')
-		z_up = para.get('z_up')
-		# One dimentional case
-		OneDx = ((y_fix and z_fix) or not(y_fix or z_fix)) and (x_low and x_up) # varying x
-		OneDz = ((y_fix and x_fix) or not(y_fix or x_fix)) and (z_low and z_up) # varying z
-		OneDy = ((z_fix and x_fix) or not(z_fix or x_fix)) and (y_low and y_up) # varying y
-		# Two dimensional case (Two Dimensional Part Still Pending)
-		TwoDxy = z_fix and ((x_low and x_up) and (y_low and y_up))
-		TwoDxz = y_fix and ((x_low and x_up) and (z_low and z_up))
-		TwoDyz = x_fix and ((y_low and y_up) and (z_low and z_up))
-		AA = zeros(101)
-		xx = zeros(101)
-		if OneDx:
-			xx = linspace(x_low,x_up, 101)
-			for i,value in enumerate(xx):
-				AA[i] = self.evaluate(x=value)
-			plt.plot(xx,AA)
-			plt.show()
-			return
-		if OneDy:
-			xx = linspace(y_low,y_up, 101)
-			for i,value in enumerate(xx):
-				AA[i] = self.evaluate(y=value)
-			plt.plot(xx,AA)
-			plt.show()
-			return
-		if OneDz:
-			xx = linspace(z_low,z_up, 101)
-			for i,value in enumerate(xx):
-				AA[i] = self.evaluate(z=value)
-			plt.plot(xx,AA)
-			plt.show()
-			return
+	def generate_figure(self, x_low, x_up, n=101, **para):
+		xx = linspace(x_low, x_up, n)
+		y = para.get('y')
+		if not y:
+			y = 0
+		z = para.get('z')
+		if not z:
+			z = 0
+		yy = array([self.evaluate(x=x,y=y,z=z) for x in xx])
+		plt.plot(xx,yy)
+		plt.show()
+class PiecewiseContinousPolynomialFunction(ContinuousPolynomialFunction):
+	"""docstring for Piece wise ContinousPolynomialFunction"""
+	def __init__(self, num_pieces, limits, **para):
+		self.num_pieces = num_pieces
+		self.pieces = {}
+		self.limits = limits
+		for piece in range(num_pieces):
+			self.pieces[piece] = super().__init__(C=para.get('C{0:d}'.format(piece+1)),\
+												  D=para.get('C{0:d}'.format(piece+1)),\
+												  E=para.get('C{0:d}'.format(piece+1)),\
+												  CL=para.get('C{0:d}'.format(piece+1)),\
+												  DL=para.get('C{0:d}'.format(piece+1)),\
+												  EL=para.get('C{0:d}'.format(piece+1)))
+	def evaluate(self,**para):
+		x = para.get('x')
+		if not x:
+			x = 0
+		y = para.get('y')
+		if not y:
+			y = 0
+		z = para.get('z')
+		if not z:
+			z = 0
+		for i,limit in enumerate(self.limits):
+			if x <= limit:
+				return self.pieces[i].evaluate(x=x, y=y, z=z)
+				break
+	def generate_figure(self, x_low, x_up,y,z, n = 101):
+		pass
+
+def tests():
+	pass
 
 if __name__ == '__main__':
 	print "This is a class for Continuous Polynomial Function"
